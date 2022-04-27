824fa463683ca2b76864124beff5b4a4947d0cdd
Jason Williams <jasonw@jhu.edu>
2022-04-27 13:50:16 -0400

    Cleanup __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,4 @@
-# from .dhcp_delete import dhcp_delete
-# from .dhcp_update import dhcp_update
-# from .dns_delete import dns_delete
-# from .dns_update import dns_update
-# from .image_delete import image_delete
-# from .image_update import image_update
-# from .pxe_delete import pxe_delete
-# from .pxe_update import pxe_update
-# from .repo_delete import repo_delete
-# from .repo_update import repo_update
+
 from os.path import dirname, basename, isfile, join
 import glob
 modules = glob.glob(join(dirname(__file__), "*.py"))
