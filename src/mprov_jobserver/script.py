9f4994b837b70c2c1aa62561954b0da4abe9eaaa
Jason Williams <jasonw@jhu.edu>
2022-11-03 11:50:25 -0400

    Adding an MProvScript class for script-runner scripts.

---
+++ src/mprov_jobserver/script.py
@@ -0,0 +1,88 @@
+
+import requests
+import socket
+import uuid
+import yaml, os, sys, glob, time
+class MProvScript():
+  '''
+  This class is a template that can be used by scripts run
+  by the 'script-runner' to have access to the jobserver 
+  config to run against the API.
+  '''
+  config_data = {}
+  mprovURL = "http://127.0.0.1:8080/"
+  apikey = ""
+  heartbeatInterval = 10
+  runonce = False
+  sessionOk = False
+  disklayout = {}
+  session = requests.Session()
+  ip_address = None
+  configfile = "/etc/mprov/jobserver.yaml"
+  def __init__(self, **kwargs):
+    # load our config
+    self.load_config()
+
+    # start a session
+    if not self.startSession():
+      print("Error: Unable to communicate with the mPCC.")
+      sys.exit(1)
+
+  def yaml_include(self, loader, node):
+    # disable includes, no need.
+    return {}
+
+  def load_config(self):
+    # load the config yaml
+    # print(self.configfile)
+    yaml.add_constructor("!include", self.yaml_include)
+    
+    if not(os.path.isfile(self.configfile) and os.access(self.configfile, os.R_OK)):
+      print("Error: Unable to find a working config file.")
+      sys.exit(1)
+
+    with open(self.configfile, "r") as yamlfile:
+      self.config_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+    # flatten the config space
+    result = {}
+    for entry in self.config_data:
+      result.update(entry)
+    self.config_data = result
+
+    # map the global config on to our object
+    for config_entry in self.config_data['global'].keys():
+      try:
+        getattr(self, config_entry)
+        setattr(self, config_entry, self.config_data['global'][config_entry])
+      except:
+        # ignore unused keys
+        pass
+    pass
+
+  def startSession(self):
+    
+    self.session.headers.update({
+      'Authorization': 'Api-Key ' + self.apikey,
+      'Content-Type': 'application/json'
+      })
+
+    # connect to the mPCC
+    try:
+      response = self.session.get(self.mprovURL, stream=True)
+    except:
+      print("Error: Communication error to the server.  Retrying.", file=sys.stderr)
+      self.sessionOk = False
+      time.sleep(self.heartbeatInterval)
+      return False
+    self.sessionOk = True
+    # get the sock from the session
+    s = socket.fromfd(response.raw.fileno(), socket.AF_INET, socket.SOCK_STREAM)
+    # get the address from the socket
+    address = s.getsockname()
+    self.ip_address=address
+      
+    # if we get a response.status_code == 200, we're ok.  If not,
+    # our auth failed.
+    return response.status_code == 200
+
