927532a569c2989e38ebfcd85ede47ba7f801f9e
Jason Williams <jasonw@jhu.edu>
2024-12-12 06:23:23 -0500

    add the ability template the config via env

---
+++ start_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MPROV_ARGS=${MPROV_ARGS:="-c /etc/mprov/jobserver.yaml"}
+# let's template our config out based on env vars
+python3 -c 'import os
+import sys
+import jinja2
+sys.stdout.write(jinja2.Template(sys.stdin.read()).render(env=os.environ))
+' <  /etc/mprov/jobserver.yaml.j2 > /etc/mprov/jobserver.yaml
+
+/usr/local/bin/mprov_jobserver ${MPROV_ARGS}
